1079d32947de6556486db9065e6b698fd3df4a95
snehasarathi <148054348+snehasarathi@users.noreply.github.com>
2023-10-16 15:10:18 +0530

    Add files via upload

---
+++ challage2.1.py.py
@@ -0,0 +1,19 @@
+def __init__(self, account_number, account_holder_name,initial_balance=0=0.0):
+  self.__account_number=account_number
+  self.__account_holder_name=account_holder_name
+  self.__account_balance=initial_balance
+  def deposit(self, amount ):
+    if amount>0:
+      self.__account_balance+=amount
+      print("Deposited £{}.New balance:£{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.please deposit a positive amount.")
+      def withdraw(self, amount ):
+        if amount>0and amount<=self.__account_balance
+        self.__account_balance_=amount
+print("withdraw ₹{}.new balance:₹{}".format amount,self.__account_balance))
+else:
+  print ("Invalid withdraw ( amount or insufficient balance.")
+  def display _ balance (Self):
+      
+  
